d6a50fab2430804df501f6983a8d1a0bebaa49f7
Julian Andres Klode <julian.klode@canonical.com>
2024-06-20 22:05:17 +0200

    Improve elliptic curve variable naming

--- apt-pkg/contrib/openpgp.cc
+++ apt-pkg/contrib/openpgp.cc
@@ -81,11 +81,12 @@ constexpr char algorithms[][8] = {
    "EdDSA",
 };
 
+// All the known elliptic curves
 constexpr struct
 {
    const char *name;
    const unsigned char oidhex[10];
-} ELLIP_CURVES[] = {
+} ellipticCurves[] = {
    {"NIST P-256", {0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x3, 0x1, 0x7, 0, 0}},
    {"NIST P-384", {0x2B, 0x81, 0x04, 0x00, 0x22, 0, 0, 0, 0, 0}},
    {"NIST P-521", {0x2B, 0x81, 0x04, 0x00, 0x23, 0, 0, 0, 0, 0}},
@@ -134,11 +135,11 @@ static bool VerifyPublicKeyPacket(const char *path, APT::StringView key, APT::Op
       {
 	 int oidlen = key[6] & 0xFF;
 	 auto oid = key.substr(7, oidlen);
-	 for (size_t i = 0; i < APT_ARRAY_SIZE(ELLIP_CURVES); i++)
+	 for (size_t i = 0; i < APT_ARRAY_SIZE(ellipticCurves); i++)
 	 {
-	    if (memcmp(ELLIP_CURVES[i].oidhex, oid.data(), oid.size()) != 0)
+	    if (memcmp(ellipticCurves[i].oidhex, oid.data(), oid.size()) != 0)
 	       continue;
-	    strprintf(out.algorithm, "%s-%s", algorithms[algo], ELLIP_CURVES[i].name);
+	    strprintf(out.algorithm, "%s-%s", algorithms[algo], ellipticCurves[i].name);
 	    return true;
 	 }
 	 std::string oidhex;
